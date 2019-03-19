e64e0fb9099d9da9b6efc5ebc651e0f4005637bd
supun-kanda <supunkandambige9@gmail.com>
2019-03-19 10:50:50 +0530

    Add Class to S1

--- S1.py
+++ S1.py
@@ -1,4 +1,13 @@
+class A():
+	__prNum = 5
+	def __init__(self, number, string):
+		self.number = number
+		self.string = string
 a = 1
 b = 2
 
+obj = A(4,'Supun')
+
 print(a,b)
+print(obj.string)
+
