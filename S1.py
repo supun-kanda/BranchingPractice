c4bf6a9334038ac0cb9495327acbc9faf0fe7054
supun-kanda <supunkandambige9@gmail.com>
2019-03-19 15:39:30 +0530

    add property option

--- S1.py
+++ S1.py
@@ -1,4 +1,22 @@
+class B():
+	def __init__(self, number):
+		self.number = number
+
+	@property
+	def prStr(self):
+		return "prStr is Private. Value is %s"%self.__prStr
+	@prStr.setter
+	def prStr(self, string='default'):
+		if(type(string)==str):
+			self.__prStr = string
+		else:
+			raise ValueError('Value is not a String')
+
+
+
 a = 1
 b = 2
-
+c = B(7)
+c.prStr = 'hi' 
 print(a,b)
+print(c.prStr)
